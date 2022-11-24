1452a4c0f0e0af3ef6b76d6df966c2ea7283499d
xylaa <ravensvenix@gmail.com>
2022-11-25 06:37:03 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -160,7 +160,7 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','?','#','-']
+global.prefa = ['','!','.','?','#','-','`','+','/']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
